37cbca7643436dc05020024f662071566cb1591c
Leo <efrenn98@gmail.com>
2021-06-02 14:54:09 -0700

    Add rating feature

---
+++ javascripts/global.js
@@ -0,0 +1,1 @@
+//all varivables files will need and getter functions
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,14 +1,14 @@
-const searchBar = document.getElementById("searchBar");
-searchBar.addEventListener("keyup", e=>{
-    const searchString = e.target.value.toLowerCase();
-    const filter = artist.filter(character =>{
-        return(
-            artist.name.toLowerCase().includes(searchString)||
-            artist.song.toLowerCase().includes(searchString)
-        );
-    });
-    displayArtists(filter);
-});
+// const searchBar = document.getElementById("searchBar");
+// searchBar.addEventListener("keyup", e=>{
+//     const searchString = e.target.value.toLowerCase();
+//     const filter = artist.filter(character =>{
+//         return(
+//             artist.name.toLowerCase().includes(searchString)||
+//             artist.song.toLowerCase().includes(searchString)
+//         );
+//     });
+//     displayArtists(filter);
+// });
 document.getElementById('startButton').addEventListener("click", searchStuff);
 
 function searchStuff(){
@@ -24,4 +24,37 @@ function searchStuff(){
 
 
     }
-
+    function turnStarsYellow(){
+        if (document.getElementById('s5').style.backgroundColor=="yellow"){
+        document.getElementById('s4').style.backgroundColor= "yellow" 
+            if (document.getElementById('s4').style.backgroundColor= "yellow"){
+            document.getElementById('s3').style.backgroundColor= "yellow" 
+                if (document.getElementById('s3').style.backgroundColor=="yellow"){
+                    document.getElementById('s2').style.backgroundColor= "yellow" 
+                    if (document.getElementById('s2').style.backgroundColor= "yellow"){
+                            document.getElementById('s1').style.backgroundColor= "yellow" 
+                    }
+                }
+            }
+        }
+    }
+document.getElementById('s1').addEventListener("click", rateOne);
+function rateOne(){
+    Song.rating=1
+}
+document.getElementById('s2').addEventListener("click", rateTwo);
+function rateTwo(){
+    Song.rating=2
+}
+document.getElementById('s3').addEventListener("click", rateThree);
+function rateThree(){
+    Song.rating=3
+}
+document.getElementById('s4').addEventListener("click", rateFour);
+function rateFour(){
+    Song.rating=4
+}
+document.getElementById('s5').addEventListener("click", rateFive);
+function rateFive(){
+    Song.rating=5
+}

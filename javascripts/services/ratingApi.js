f366a8e837df49eff994097fb095810f87b91afc
Leo <efrenn98@gmail.com>
2021-06-17 00:31:30 -0700

    Fix styling

--- javascripts/services/ratingApi.js
+++ javascripts/services/ratingApi.js
@@ -1,6 +1,6 @@
 class RatingApi {
    static createRatings(e) {
-        const userName
+        const userName= document.getElementById('welcome-users').innerText.split(" ")[1]
         const artistName =e.target.parentElement.parentElement.querySelector("#artists-list .artist h2").innerText
         const findArtist= Artist.findByName(artistName)
         const findUser= User.findByName(userName)
@@ -8,23 +8,28 @@ class RatingApi {
         const scoreMap= {"1":5, "2":4, "3":3,"4":2,"5":1}
         const data = {
             score: scoreMap[e.target.value],
-            artist_id: findArtist.id
+            artist_id: findArtist.id,
             user_id: findUser.id
         }
         fetch("http://localhost:3000/ratings", {
+            
             method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         })
         .then(resp => resp.json())
         .then(json => { 
             new Rating(json)
+            
             const artist= Artist.findById(json.artist_id)
             document.querySelector(".avg-rating").innerText=`Rating: ${artist.avgRating()}`
             document.getElementById('star-widget').style.visibility = "hidden";
-
+            
         })
+        .catch(this.handleError)
+        
+
     }
 }
